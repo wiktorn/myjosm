wiktorn/myjosm

commit f73b5927f56094b2222e85b023e89a20d0e0a3ec

applied #8987 - immutable coordinates (patch by shinigami)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6162 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/coor/Coordinate.java
@@ -1,7 +1,6 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.data.coor;
 
-import java.awt.geom.Point2D;
 import java.io.Serializable;
 
 /**
@@ -16,10 +15,10 @@ import java.io.Serializable;
  *
  * @author imi
  */
-abstract class Coordinate extends Point2D implements Serializable {
+abstract class Coordinate implements Serializable {
 
-    protected double x;
-    protected double y;
+    protected final double x;
+    protected final double y;
 
     /**
      * Construct the point with latitude / longitude values.
@@ -31,20 +30,12 @@ abstract class Coordinate extends Point2D implements Serializable {
         this.x = x; this.y = y;
     }
 
-    @Override
     public double getX() {
         return x;
     }
 
-    @Override
     public double getY() {
         return y;
-    }
-
-    @Override
-    public void setLocation (double x, double y) {
-        this.x = x;
-        this.y = y;
     }
 
     @Override

src/org/openstreetmap/josm/actions/mapmode/ExtrudeAction.java
@@ -683,7 +683,7 @@ public class ExtrudeAction extends MapMode implements MapViewPaintable {
                 Point p3 = mv.getPoint(newN1en);
                 Point p4 = mv.getPoint(newN2en);
 
-                EastNorth normalUnitVector = getNormalUniVector();
+                Point2D normalUnitVector = getNormalUniVector();
 
                 if (mode == Mode.extrude || mode == Mode.create_new) {
                     g2.setColor(mainColor);
@@ -752,16 +752,16 @@ public class ExtrudeAction extends MapMode implements MapViewPaintable {
         }
     }
 
-    private EastNorth getNormalUniVector() {
-        double fac = 1.0 / activeMoveDirection.en.distance(0,0);
+    private Point2D getNormalUniVector() {
+        double fac = 1.0 / activeMoveDirection.en.length();
         // mult by factor to get unit vector.
-        EastNorth normalUnitVector = new EastNorth(activeMoveDirection.en.getX() * fac, activeMoveDirection.en.getY() * fac);
+        Point2D normalUnitVector = new Point2D.Double(activeMoveDirection.en.getX() * fac, activeMoveDirection.en.getY() * fac);
 
         // Check to see if our new N1 is in a positive direction with respect to the normalUnitVector.
         // Even if the x component is zero, we should still be able to discern using +0.0 and -0.0
         if (newN1en != null && ((newN1en.getX() > initialN1en.getX()) != (normalUnitVector.getX() > -0.0))) {
             // If not, use a sign-flipped version of the normalUnitVector.
-            normalUnitVector = new EastNorth(-normalUnitVector.getX(), -normalUnitVector.getY());
+            normalUnitVector = new Point2D.Double(-normalUnitVector.getX(), -normalUnitVector.getY());
         }
 
         //HACK: swap Y, because the target pixels are top down, but EastNorth is bottom-up.
@@ -770,7 +770,7 @@ public class ExtrudeAction extends MapMode implements MapViewPaintable {
         return normalUnitVector;
     }
 
-    private void drawAngleSymbol(Graphics2D g2, Point2D center, EastNorth normal, boolean mirror) {
+    private void drawAngleSymbol(Graphics2D g2, Point2D center, Point2D normal, boolean mirror) {
         // EastNorth units per pixel
         double factor = 1.0/g2.getTransform().getScaleX();
         double raoffsetx = symbolSize*factor*normal.getX();

src/org/openstreetmap/josm/command/Command.java
@@ -15,7 +15,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.PrimitiveData;
@@ -221,8 +220,7 @@ abstract public class Command extends PseudoCommand {
 
     private static boolean isOutlying(OsmPrimitive osm, Area area) {
         if (osm instanceof Node && !osm.isNewOrUndeleted()) {
-            LatLon coor = ((Node) osm).getCoor();
-            return coor != null && !area.contains(coor);
+            return !((Node) osm).getCoor().isIn(area);
         } else if (osm instanceof Way) {
             for (Node n : ((Way) osm).getNodes()) {
                 if (isOutlying(n, area)) {

src/org/openstreetmap/josm/command/SequenceCommand.java
@@ -6,7 +6,6 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 
 import javax.swing.Icon;
 

src/org/openstreetmap/josm/data/Bounds.java
@@ -205,7 +205,7 @@ public class Bounds {
         if (crosses180thMeridian()) {
             LatLon result = new LatLon(minLat, minLon-360.0).getCenter(getMax());
             if (result.lon() < -180.0) {
-                result.setLocation(result.lon()+360.0, result.lat());
+                result = new LatLon(result.lat(), result.lon() + 360.0);
             }
             return result;
         } else {

src/org/openstreetmap/josm/data/coor/CachedLatLon.java
@@ -32,18 +32,6 @@ public class CachedLatLon extends LatLon {
         this.eastNorth = eastNorth;
     }
 
-    public final void setCoor(LatLon coor) {
-        setLocation(coor.lon(), coor.lat());
-        proj = null;
-    }
-
-    public final void setEastNorth(EastNorth eastNorth) {
-        proj = Main.getProjection();
-        this.eastNorth = eastNorth;
-        LatLon l = proj.eastNorth2latlon(eastNorth);
-        setLocation(l.lon(), l.lat());
-    }
-
     /**
      * Replies the projected east/north coordinates.
      *

src/org/openstreetmap/josm/data/coor/EastNorth.java
@@ -43,10 +43,39 @@ public class EastNorth extends Coordinate {
         return new EastNorth((this.x + en2.x)/2.0, (this.y + en2.y)/2.0);
     }
 
-    public double distance(EastNorth en2) {
-        return Math.sqrt((this.x-en2.x)*(this.x-en2.x) + (this.y-en2.y)*(this.y-en2.y));
+    /**
+     * Counts euclidean distance between this and other EastNorth.
+     * 
+     * @param en2 other EastNorth
+     * @return distance between this and other EastNorth
+     */
+    public double distance(final EastNorth en2) {
+        final double dx = this.x-en2.x;
+        final double dy = this.y-en2.y;
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
+    /**
+     * Counts square of euclidean distance between this and other EastNorth.
+     * 
+     * @param en2 other EastNorth
+     * @return square of distance between this and other EastNorth
+     */
+    public double distanceSq(final EastNorth en2) {
+        final double dx = this.x-en2.x;
+        final double dy = this.y-en2.y;
+        return dx*dx + dy*dy;
+    }
+
+    /**
+     * Counts length (distance from [0,0]) of this.
+     * 
+     * @return length of this
+     */
+    public double length(){
+        return Math.sqrt(x*x + y*y);
+    }
+    
     /**
      * Returns the heading, in radians, that you have to use to get from
      * this EastNorth to another. Heading is mapped into [0, 2pi)

src/org/openstreetmap/josm/data/coor/LatLon.java
@@ -10,6 +10,7 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.toRadians;
 import static org.openstreetmap.josm.tools.I18n.trc;
 
+import java.awt.geom.Area;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -221,6 +222,16 @@ public class LatLon extends Coordinate {
     }
 
     /**
+     * Check if this is contained in given area or area is null. 
+     * 
+     * @param a Area
+     * @return <code>true</code> if this is contained in given area or area is null. 
+     */
+    public boolean isIn(Area a) {
+        return a == null || a.contains(x, y);
+    }
+
+    /**
      * Computes the distance between this lat/lon and another point on the earth.
      * Uses Haversine formular.
      * @param other the other point.
@@ -283,6 +294,18 @@ public class LatLon extends Coordinate {
 
     public LatLon getCenter(LatLon ll2) {
         return new LatLon((this.lat() + ll2.lat())/2.0, (this.lon() + ll2.lon())/2.0);
+    }
+
+    /**
+     * Counts euclidean distance between this and other LatLon.
+     * 
+     * @param ll2 other LatLon
+     * @return distance between this and other LatLon
+     */
+   public double distance(final LatLon ll2) {
+        final double dx = this.x-ll2.x;
+        final double dy = this.y-ll2.y;
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
     @Override public String toString() {

commit 16471f102f65a0937b9423d04d75e71423969cac

fix #8987 - fix Coordinate.equals() broken in r6162
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6163 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/data/coor/Coordinate.java
@@ -54,8 +54,6 @@ abstract class Coordinate implements Serializable {
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (!super.equals(obj))
-            return false;
         if (getClass() != obj.getClass())
             return false;
         Coordinate other = (Coordinate) obj;
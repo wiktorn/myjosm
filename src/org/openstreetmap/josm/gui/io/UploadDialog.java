wiktorn/myjosm

commit 9c7baf913e1316c0a35a825cec80a2a24dcfa6e4

see #3764 - make UI messages copy-able (patch by simon04)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6901 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/io/UploadStrategySelectionPanel.java
@@ -1,5 +1,6 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.io;
+
 import static org.openstreetmap.josm.tools.I18n.tr;
 import static org.openstreetmap.josm.tools.I18n.trn;
 
@@ -29,10 +30,9 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.io.OsmApi;
-import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.gui.widgets.JosmTextField;
+import org.openstreetmap.josm.io.OsmApi;
 
 /**
  * UploadStrategySelectionPanel is a panel for selecting an upload strategy.
@@ -107,7 +107,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         gc.weightx = 0.0;
         gc.weighty = 0.0;
         gc.gridwidth = 2;
-        JLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
+        JMultilineLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
         lbl.setText(tr("Upload data in one request"));
         pnl.add(lbl, gc);
         gc.gridx = 3;
@@ -319,8 +319,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         int maxChunkSize = OsmApi.getOsmApi().getCapabilities().getMaxChangesetSize();
         if (maxChunkSize > 0 && numUploadedObjects > maxChunkSize) {
             rbStrategy.get(UploadStrategy.SINGLE_REQUEST_STRATEGY).setEnabled(false);
-            JLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
-            lbl.setIcon(ImageProvider.get("warning-small.png"));
+            JMultilineLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
             lbl.setText(tr("Upload in one request not possible (too many objects to upload)"));
             lbl.setToolTipText(tr("<html>Cannot upload {0} objects in one request because the<br>"
                     + "max. changeset size {1} on server ''{2}'' is exceeded.</html>",
@@ -340,9 +339,8 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
 
         } else {
             rbStrategy.get(UploadStrategy.SINGLE_REQUEST_STRATEGY).setEnabled(true);
-            JLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
+            JMultilineLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
             lbl.setText(tr("Upload data in one request"));
-            lbl.setIcon(null);
             lbl.setToolTipText("");
             lblNumRequests.get(UploadStrategy.SINGLE_REQUEST_STRATEGY).setVisible(true);
 

src/org/openstreetmap/josm/Main.java
@@ -36,7 +36,6 @@ import javax.swing.Action;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -87,6 +86,7 @@ import org.openstreetmap.josm.gui.preferences.projection.ProjectionPreference;
 import org.openstreetmap.josm.gui.progress.PleaseWaitProgressMonitor;
 import org.openstreetmap.josm.gui.progress.ProgressMonitorExecutor;
 import org.openstreetmap.josm.gui.util.RedirectInputMap;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.io.OsmApi;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.I18n;
@@ -1122,7 +1122,7 @@ public abstract class Main {
                     version.startsWith("1.9") || version.startsWith("9"))
                 return;
             if (version.startsWith("1.5") || version.startsWith("5")) {
-                JLabel ho = new JLabel("<html>"+
+                JMultilineLabel ho = new JMultilineLabel("<html>"+
                         tr("<h2>JOSM requires Java version 6.</h2>"+
                                 "Detected Java version: {0}.<br>"+
                                 "You can <ul><li>update your Java (JRE) or</li>"+

src/org/openstreetmap/josm/actions/AboutAction.java
@@ -18,6 +18,7 @@ import javax.swing.JTabbedPane;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.Version;
 import org.openstreetmap.josm.gui.util.GuiHelper;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.gui.widgets.JosmTextArea;
 import org.openstreetmap.josm.gui.widgets.UrlLabel;
 import org.openstreetmap.josm.plugins.PluginHandler;
@@ -73,16 +74,17 @@ public class AboutAction extends JosmAction {
         license.setCaretPosition(0);
 
         JPanel info = new JPanel(new GridBagLayout());
-        JLabel caption = new JLabel("JOSM – " + tr("Java OpenStreetMap Editor"));
-        caption.setFont(GuiHelper.getTitleFont());
-        info.add(caption, GBC.eol().fill(GBC.HORIZONTAL).insets(10,0,0,0));
-        info.add(GBC.glue(0,10), GBC.eol());
-        info.add(new JLabel(tr("Version {0}", version.getVersionString())), GBC.eol().fill(GBC.HORIZONTAL).insets(10,0,0,0));
-        info.add(GBC.glue(0,5), GBC.eol());
-        info.add(new JLabel(tr("Last change at {0}",version.getTime())), GBC.eol().fill(GBC.HORIZONTAL).insets(10,0,0,0));
-        info.add(GBC.glue(0,5), GBC.eol());
-        info.add(new JLabel(tr("Java Version {0}",System.getProperty("java.version"))), GBC.eol().fill(GBC.HORIZONTAL).insets(10,0,0,0));
-        info.add(GBC.glue(0,10), GBC.eol());
+        final JMultilineLabel label = new JMultilineLabel("<html>" +
+                "<h1>" + "JOSM – " + tr("Java OpenStreetMap Editor") + "</h1>" +
+                "<p style='font-size:75%'></p>" +
+                "<p>" + tr("Version {0}", version.getVersionString()) + "</p>" +
+                "<p style='font-size:50%'></p>" +
+                "<p>" + tr("Last change at {0}", version.getTime()) + "</p>" +
+                "<p style='font-size:50%'></p>" +
+                "<p>" + tr("Java Version {0}", System.getProperty("java.version")) + "</p>" +
+                "<p style='font-size:50%'></p>" +
+                "</html>");
+        info.add(label, GBC.eol().fill(GBC.HORIZONTAL).insets(10, 0, 0, 0));
         info.add(new JLabel(tr("Homepage")), GBC.std().insets(10,0,10,0));
         info.add(new UrlLabel(Main.getJOSMWebsite(),2), GBC.eol().fill(GBC.HORIZONTAL));
         info.add(GBC.glue(0,5), GBC.eol());

src/org/openstreetmap/josm/command/Command.java
@@ -9,7 +9,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -25,6 +24,7 @@ import org.openstreetmap.josm.data.osm.visitor.AbstractVisitor;
 import org.openstreetmap.josm.gui.ConditionalOptionPaneUtil;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 
 /**
@@ -209,7 +209,7 @@ public abstract class Command extends PseudoCommand {
         }
         if (outside) {
             JPanel msg = new JPanel(new GridBagLayout());
-            msg.add(new JLabel("<html>" + outsideDialogMessage + "</html>"));
+            msg.add(new JMultilineLabel("<html>" + outsideDialogMessage + "</html>"));
             boolean answer = ConditionalOptionPaneUtil.showConfirmationDialog(
                     operation + "_outside_nodes",
                     Main.parent,
@@ -223,7 +223,7 @@ public abstract class Command extends PseudoCommand {
         }
         if (incomplete) {
             JPanel msg = new JPanel(new GridBagLayout());
-            msg.add(new JLabel("<html>" + incompleteDialogMessage + "</html>"));
+            msg.add(new JMultilineLabel("<html>" + incompleteDialogMessage + "</html>"));
             boolean answer = ConditionalOptionPaneUtil.showConfirmationDialog(
                     operation + "_incomplete",
                     Main.parent,

src/org/openstreetmap/josm/command/DeleteCommand.java
@@ -19,7 +19,6 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.swing.Icon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -37,6 +36,7 @@ import org.openstreetmap.josm.gui.ConditionalOptionPaneUtil;
 import org.openstreetmap.josm.gui.DefaultNameFormatter;
 import org.openstreetmap.josm.gui.actionsupport.DeleteFromRelationConfirmationDialog;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
@@ -463,7 +463,7 @@ public class DeleteCommand extends Command {
 
     private static boolean confirmRelationDeletion(Collection<Relation> relations) {
         JPanel msg = new JPanel(new GridBagLayout());
-        msg.add(new JLabel("<html>" + trn(
+        msg.add(new JMultilineLabel("<html>" + trn(
                 "You are about to delete {0} relation: {1}"
                 + "<br/>"
                 + "This step is rarely necessary and cannot be undone easily after being uploaded to the server."

src/org/openstreetmap/josm/gui/ConditionalOptionPaneUtil.java
@@ -12,12 +12,12 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.swing.ButtonGroup;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.Utils;
 
@@ -270,7 +270,7 @@ public final class ConditionalOptionPaneUtil {
             if (message instanceof Component) {
                 add((Component) message, GBC.eop());
             } else if (message != null) {
-                add(new JLabel(message.toString()), GBC.eop());
+                add(new JMultilineLabel(message.toString()), GBC.eop());
             }
             add(cbShowPermanentDialog, GBC.eol());
             add(cbShowSessionDialog, GBC.eol());

src/org/openstreetmap/josm/gui/SplashScreen.java
@@ -64,7 +64,7 @@ public class SplashScreen extends JFrame {
         innerContentPane.add(logo, gbc);
 
         // Add the name of this application
-        JLabel caption = new JLabel("JOSM - " + tr("Java OpenStreetMap Editor"));
+        JLabel caption = new JLabel("JOSM – " + tr("Java OpenStreetMap Editor"));
         caption.setFont(GuiHelper.getTitleFont());
         gbc.gridheight = 1;
         gbc.gridx = 1;
@@ -106,6 +106,10 @@ public class SplashScreen extends JFrame {
         });
     }
 
+    /**
+     * Returns the progress monitor.
+     * @return The progress monitor
+     */
     public ProgressMonitor getProgressMonitor() {
         return progressMonitor;
     }

src/org/openstreetmap/josm/gui/io/BasicUploadSettingsPanel.java
@@ -28,7 +28,7 @@ import javax.swing.event.HyperlinkListener;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.osm.Changeset;
 import org.openstreetmap.josm.gui.widgets.HistoryComboBox;
-import org.openstreetmap.josm.gui.widgets.JosmEditorPane;
+import org.openstreetmap.josm.gui.widgets.JMultilineLabel;
 import org.openstreetmap.josm.tools.CheckParameterUtil;
 import org.openstreetmap.josm.tools.GBC;
 
@@ -55,8 +55,7 @@ public class BasicUploadSettingsPanel extends JPanel {
     protected JPanel buildUploadCommentPanel() {
         JPanel pnl = new JPanel(new GridBagLayout());
 
-        final JEditorPane commentLabel = JosmEditorPane.createJLabelLikePane();
-        commentLabel.setText("<html><b>" + tr("Provide a brief comment for the changes you are uploading:"));
+        final JEditorPane commentLabel = new JMultilineLabel("<html><b>" + tr("Provide a brief comment for the changes you are uploading:"));
         pnl.add(commentLabel, GBC.eol().insets(0, 5, 10, 3).fill(GBC.HORIZONTAL));
         hcbUploadComment.setToolTipText(tr("Enter an upload comment"));
         hcbUploadComment.setMaxTextLength(Changeset.MAX_COMMENT_LENGTH);
@@ -68,8 +67,7 @@ public class BasicUploadSettingsPanel extends JPanel {
         hcbUploadComment.getEditor().getEditorComponent().addFocusListener(commentModelListener);
         pnl.add(hcbUploadComment, GBC.eol().fill(GBC.HORIZONTAL));
 
-        final JEditorPane sourceLabel = JosmEditorPane.createJLabelLikePane();
-        sourceLabel.setText("<html><b>" + tr("Specify the data source for the changes")
+        final JEditorPane sourceLabel = new JMultilineLabel("<html><b>" + tr("Specify the data source for the changes")
                 + "</b> (<a href=\"urn:changeset-source\">" + tr("obtain from current layers") + "</a>)<b>:</b>");
         sourceLabel.addHyperlinkListener(new HyperlinkListener() {
             @Override

commit 2c261bf85961565b01d297ffd03db0d25642a030

fix #10362 - wrong layout in advanced tab of upload dialog (regression from r6901)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7375 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/io/UploadDialog.java
@@ -233,6 +233,8 @@ public class UploadDialog extends AbstractUploadDialog implements PropertyChange
                 }
         );
 
+        setMinimumSize(new Dimension(300, 350));
+
         Main.pref.addPreferenceChangeListener(this);
     }
 

src/org/openstreetmap/josm/gui/io/UploadStrategySelectionPanel.java
@@ -104,7 +104,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(rbStrategy.get(UploadStrategy.SINGLE_REQUEST_STRATEGY), gc);
         gc.gridx = 1;
         gc.gridy = 1;
-        gc.weightx = 0.0;
+        gc.weightx = 1.0;
         gc.weighty = 0.0;
         gc.gridwidth = 2;
         JMultilineLabel lbl = lblStrategies.get(UploadStrategy.SINGLE_REQUEST_STRATEGY);
@@ -112,7 +112,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(lbl, gc);
         gc.gridx = 3;
         gc.gridy = 1;
-        gc.weightx = 1.0;
+        gc.weightx = 0.0;
         gc.weighty = 0.0;
         gc.gridwidth = 1;
         pnl.add(lblNumRequests.get(UploadStrategy.SINGLE_REQUEST_STRATEGY), gc);
@@ -125,7 +125,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(rbStrategy.get(UploadStrategy.CHUNKED_DATASET_STRATEGY), gc);
         gc.gridx = 1;
         gc.gridy = 2;
-        gc.weightx = 0.0;
+        gc.weightx = 1.0;
         gc.weighty = 0.0;
         gc.gridwidth = 1;
         lbl = lblStrategies.get(UploadStrategy.CHUNKED_DATASET_STRATEGY);
@@ -139,7 +139,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(tfChunkSize = new JosmTextField(4), gc);
         gc.gridx = 3;
         gc.gridy = 2;
-        gc.weightx = 1.0;
+        gc.weightx = 0.0;
         gc.weighty = 0.0;
         gc.gridwidth = 1;
         pnl.add(lblNumRequests.get(UploadStrategy.CHUNKED_DATASET_STRATEGY), gc);
@@ -152,7 +152,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(rbStrategy.get(UploadStrategy.INDIVIDUAL_OBJECTS_STRATEGY), gc);
         gc.gridx = 1;
         gc.gridy = 3;
-        gc.weightx = 0.0;
+        gc.weightx = 1.0;
         gc.weighty = 0.0;
         gc.gridwidth = 2;
         lbl = lblStrategies.get(UploadStrategy.INDIVIDUAL_OBJECTS_STRATEGY);
@@ -160,7 +160,7 @@ public class UploadStrategySelectionPanel extends JPanel implements PropertyChan
         pnl.add(lbl, gc);
         gc.gridx = 3;
         gc.gridy = 3;
-        gc.weightx = 1.0;
+        gc.weightx = 0.0;
         gc.weighty = 0.0;
         gc.gridwidth = 1;
         pnl.add(lblNumRequests.get(UploadStrategy.INDIVIDUAL_OBJECTS_STRATEGY), gc);

src/org/openstreetmap/josm/gui/widgets/JosmTextField.java
@@ -29,6 +29,10 @@ public class JosmTextField extends JTextField {
     public JosmTextField(Document doc, String text, int columns) {
         super(doc, text, columns);
         TextContextualPopupMenu.enableMenuFor(this);
+        // Fix minimum size when columns are specified
+        if (columns > 0) {
+            setMinimumSize(getPreferredSize());
+        }
     }
 
     /**
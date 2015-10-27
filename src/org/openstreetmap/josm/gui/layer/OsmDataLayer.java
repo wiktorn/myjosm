wiktorn/myjosm

commit 7472a80cb94b8394d6dc95457f2b959d2067d79d

fix #9012 - Saving a converted osm/gpx layer should save with correct file extension preselected
git-svn-id: https://josm.openstreetmap.de/svn/trunk@8830 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/OsmDataLayer.java
@@ -742,6 +742,7 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, S
         public void actionPerformed(ActionEvent e) {
             final GpxData data = toGpxData();
             final GpxLayer gpxLayer = new GpxLayer(data, tr("Converted from: {0}", getName()));
+            gpxLayer.setAssociatedFile(new File(getAssociatedFile().getParentFile(), getAssociatedFile().getName() + ".gpx"));
             Main.main.addLayer(gpxLayer);
             if (Main.pref.getBoolean("marker.makeautomarkers", true) && !data.waypoints.isEmpty()) {
                 Main.main.addLayer(new MarkerLayer(data, tr("Converted from: {0}", getName()), null, gpxLayer));

src/org/openstreetmap/josm/gui/layer/gpx/ConvertToDataLayerAction.java
@@ -6,6 +6,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -127,7 +128,8 @@ public abstract class ConvertToDataLayerAction<T extends Layer> extends Abstract
             return;
         }
         final DataSet ds = convert();
-        final OsmDataLayer layer = new OsmDataLayer(ds, tr("Converted from: {0}", this.layer.getName()), this.layer.getAssociatedFile());
+        final OsmDataLayer layer = new OsmDataLayer(ds, tr("Converted from: {0}", this.layer.getName()), null);
+        layer.setAssociatedFile(new File(this.layer.getAssociatedFile().getParentFile(), this.layer.getAssociatedFile().getName() + ".osm"));
         layer.setUploadDiscouraged(true);
         Main.main.addLayer(layer);
         Main.main.removeLayer(this.layer);

commit 52d22f631872f25d6c730ed62ddee230705348f7

fix #12009 - Preserve filename for conversion GPX -> OSM -> GPX
Regression from r8830.

git-svn-id: https://josm.openstreetmap.de/svn/trunk@8956 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/OsmDataLayer.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Pattern;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -744,7 +745,8 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, S
         public void actionPerformed(ActionEvent e) {
             final GpxData data = toGpxData();
             final GpxLayer gpxLayer = new GpxLayer(data, tr("Converted from: {0}", getName()));
-            gpxLayer.setAssociatedFile(new File(getAssociatedFile().getParentFile(), getAssociatedFile().getName() + ".gpx"));
+            final String filename = getAssociatedFile().getName().replaceAll(Pattern.quote(".gpx.osm") + "$", "") + ".gpx";
+            gpxLayer.setAssociatedFile(new File(getAssociatedFile().getParentFile(), filename));
             Main.main.addLayer(gpxLayer);
             if (Main.pref.getBoolean("marker.makeautomarkers", true) && !data.waypoints.isEmpty()) {
                 Main.main.addLayer(new MarkerLayer(data, tr("Converted from: {0}", getName()), null, gpxLayer));
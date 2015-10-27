wiktorn/myjosm

commit 53a9e5857e8f8cb4cbdeb128506a7304c88a46dc

fix #12004 - suggest to replace `source:maxspeed=UK:*` by `source:maxspeed=GB:*` as per ISO 3166-2
git-svn-id: https://josm.openstreetmap.de/svn/trunk@8941 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/command/ChangePropertyCommand.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 import javax.swing.Icon;
 
-import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.gui.DefaultNameFormatter;
@@ -105,8 +105,10 @@ public class ChangePropertyCommand extends Command {
         }
     }
 
-    @Override public boolean executeCommand() {
-        Main.main.getCurrentDataSet().beginUpdate();
+    @Override
+    public boolean executeCommand() {
+        final DataSet dataSet = objects.get(0).getDataSet();
+        dataSet.beginUpdate();
         try {
             super.executeCommand(); // save old
 
@@ -128,11 +130,12 @@ public class ChangePropertyCommand extends Command {
             }
             return true;
         } finally {
-            Main.main.getCurrentDataSet().endUpdate();
+            dataSet.endUpdate();
         }
     }
 
-    @Override public void fillModifiedData(Collection<OsmPrimitive> modified, Collection<OsmPrimitive> deleted, Collection<OsmPrimitive> added) {
+    @Override
+    public void fillModifiedData(Collection<OsmPrimitive> modified, Collection<OsmPrimitive> deleted, Collection<OsmPrimitive> added) {
         modified.addAll(objects);
     }
 

src/org/openstreetmap/josm/data/validation/tests/Highways.java
@@ -19,6 +19,7 @@ import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.OsmUtils;
 import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.validation.FixableTestError;
 import org.openstreetmap.josm.data.validation.Severity;
 import org.openstreetmap.josm.data.validation.Test;
 import org.openstreetmap.josm.data.validation.TestError;
@@ -38,6 +39,8 @@ public class Highways extends Test {
     protected static final int SOURCE_MAXSPEED_CONTEXT_MISMATCH_VS_MAXSPEED = 2705;
     protected static final int SOURCE_MAXSPEED_CONTEXT_MISMATCH_VS_HIGHWAY = 2706;
     protected static final int SOURCE_WRONG_LINK = 2707;
+
+    protected static final String SOURCE_MAXSPEED = "source:maxspeed";
 
     /**
      * Classified highways in order of importance
@@ -91,7 +94,7 @@ public class Highways extends Test {
              && n.isReferredByWays(2)) {
                 testMissingPedestrianCrossing(n);
             }
-            if (n.hasKey("source:maxspeed")) {
+            if (n.hasKey(SOURCE_MAXSPEED)) {
                 // Check maxspeed but not context against highway for nodes
                 // as maxspeed is not set on highways here but on signs, speed cameras, etc.
                 testSourceMaxspeed(n, false);
@@ -106,7 +109,7 @@ public class Highways extends Test {
                     && w.hasKey("junction") && "roundabout".equals(w.get("junction"))) {
                 testWrongRoundabout(w);
             }
-            if (w.hasKey("source:maxspeed")) {
+            if (w.hasKey(SOURCE_MAXSPEED)) {
                 // Check maxspeed, including context against highway
                 testSourceMaxspeed(w, true);
             }
@@ -239,14 +242,20 @@ public class Highways extends Test {
     }
 
     private void testSourceMaxspeed(OsmPrimitive p, boolean testContextHighway) {
-        String value = p.get("source:maxspeed");
+        String value = p.get(SOURCE_MAXSPEED);
         if (value.matches("[A-Z]{2}:.+")) {
             int index = value.indexOf(':');
             // Check country
             String country = value.substring(0, index);
             if (!ISO_COUNTRIES.contains(country)) {
-                errors.add(new TestError(this, Severity.WARNING,
-                        tr("Unknown country code: {0}", country), SOURCE_MAXSPEED_UNKNOWN_COUNTRY_CODE, p));
+                if ("UK".equals(country)) {
+                    errors.add(new FixableTestError(this, Severity.WARNING,
+                            tr("Unknown country code: {0}", country), SOURCE_MAXSPEED_UNKNOWN_COUNTRY_CODE, p,
+                            new ChangePropertyCommand(p, SOURCE_MAXSPEED, value.replace("UK:", "GB:"))));
+                } else {
+                    errors.add(new TestError(this, Severity.WARNING,
+                            tr("Unknown country code: {0}", country), SOURCE_MAXSPEED_UNKNOWN_COUNTRY_CODE, p));
+                }
             }
             // Check context
             String context = value.substring(index+1);

test/unit/org/openstreetmap/josm/data/validation/tests/HighwaysTest.groovy
@@ -4,6 +4,7 @@ package org.openstreetmap.josm.data.validation.tests
 import org.openstreetmap.josm.JOSMFixture
 import org.openstreetmap.josm.data.coor.LatLon
 import org.openstreetmap.josm.data.osm.DataSet
+import org.openstreetmap.josm.data.osm.Node
 import org.openstreetmap.josm.data.osm.Way
 
 class HighwaysTest extends GroovyTestCase {
@@ -16,12 +17,12 @@ class HighwaysTest extends GroovyTestCase {
     public static Way createTestSetting(String highway, String highwayLink) {
         def ds = new DataSet()
 
-        def n00 = new org.openstreetmap.josm.data.osm.Node(new LatLon(0, 0))
-        def n10 = new org.openstreetmap.josm.data.osm.Node(new LatLon(1, 0))
-        def n20 = new org.openstreetmap.josm.data.osm.Node(new LatLon(2, 0))
-        def n01 = new org.openstreetmap.josm.data.osm.Node(new LatLon(0, 1))
-        def n11 = new org.openstreetmap.josm.data.osm.Node(new LatLon(1, 1))
-        def n21 = new org.openstreetmap.josm.data.osm.Node(new LatLon(2, 1))
+        def n00 = new Node(new LatLon(0, 0))
+        def n10 = new Node(new LatLon(1, 0))
+        def n20 = new Node(new LatLon(2, 0))
+        def n01 = new Node(new LatLon(0, 1))
+        def n11 = new Node(new LatLon(1, 1))
+        def n21 = new Node(new LatLon(2, 1))
 
         ds.addPrimitive(n00)
         ds.addPrimitive(n10)
@@ -59,5 +60,18 @@ class HighwaysTest extends GroovyTestCase {
         assert !Highways.isHighwayLinkOkay(createTestSetting("secondary", "primary_link"))
         assert !Highways.isHighwayLinkOkay(createTestSetting("secondary", "tertiary_link"))
         assert Highways.isHighwayLinkOkay(createTestSetting("residential", "residential"))
+    }
+
+    void testSourceMaxSpeedUnitedKingdom() {
+        def link = createTestSetting("primary", "primary")
+        link.put("maxspeed", "60 mph")
+        link.put("source:maxspeed", "UK:nsl_single")
+        def test = new Highways()
+        test.visit(link)
+        assert test.errors.size() == 1
+        def error = test.errors.get(0)
+        assert error.isFixable()
+        assert error.getFix().executeCommand()
+        assert "GB:nsl_single".equals(link.get("source:maxspeed"))
     }
 }

commit 188c95a8f7c56525280180b77570f0ac2a9f9957

fix #12019 - NPE with ChangePropertyCommand called on objects without dataset - regression from r8941
git-svn-id: https://josm.openstreetmap.de/svn/trunk@8961 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/command/ChangePropertyCommand.java
@@ -110,7 +110,9 @@ public class ChangePropertyCommand extends Command {
         if (objects.isEmpty())
             return true;
         final DataSet dataSet = objects.get(0).getDataSet();
-        dataSet.beginUpdate();
+        if (dataSet != null) {
+            dataSet.beginUpdate();
+        }
         try {
             super.executeCommand(); // save old
 
@@ -132,7 +134,9 @@ public class ChangePropertyCommand extends Command {
             }
             return true;
         } finally {
-            dataSet.endUpdate();
+            if (dataSet != null) {
+                dataSet.endUpdate();
+            }
         }
     }
 
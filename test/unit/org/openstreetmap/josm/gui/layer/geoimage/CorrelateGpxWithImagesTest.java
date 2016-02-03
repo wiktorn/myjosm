wiktorn/myjosm

commit 088a6e96210bc828912e0c09698f461b4da6f446

Deprecate `PrimaryDateParser` in favour of `DateUtils`
git-svn-id: https://josm.openstreetmap.de/svn/trunk@9383 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImages.java
@@ -80,7 +80,6 @@ import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Utils;
 import org.openstreetmap.josm.tools.date.DateUtils;
-import org.openstreetmap.josm.tools.date.PrimaryDateParser;
 import org.xml.sax.SAXException;
 
 /**
@@ -1013,7 +1012,6 @@ public class CorrelateGpxWithImages extends AbstractAction {
             GpxData gpx = gpxW.data;
 
             List<ImageEntry> imgs = getSortedImgList();
-            PrimaryDateParser dateParser = new PrimaryDateParser();
 
             // no images found, exit
             if (imgs.isEmpty()) {
@@ -1031,14 +1029,12 @@ public class CorrelateGpxWithImages extends AbstractAction {
             outer: for (GpxTrack trk : gpx.tracks) {
                 for (GpxTrackSegment segment : trk.getSegments()) {
                     for (WayPoint curWp : segment.getWayPoints()) {
-                        String curDateWpStr = curWp.getString(GpxConstants.PT_TIME);
-                        if (curDateWpStr == null) {
-                            continue;
-                        }
-
                         try {
-                            firstGPXDate = dateParser.parse(curDateWpStr).getTime()/1000;
-                            break outer;
+                            final Date parsedTime = curWp.setTimeFromAttribute();
+                            if (parsedTime != null) {
+                                firstGPXDate = parsedTime.getTime();
+                                break outer;
+                            }
                         } catch (Exception e) {
                             Main.warn(e);
                         }
@@ -1152,8 +1148,6 @@ public class CorrelateGpxWithImages extends AbstractAction {
     private int matchGpxTrack(List<ImageEntry> images, GpxData selectedGpx, long offset) {
         int ret = 0;
 
-        PrimaryDateParser dateParser = new PrimaryDateParser();
-
         for (GpxTrack trk : selectedGpx.tracks) {
             for (GpxTrackSegment segment : trk.getSegments()) {
 
@@ -1161,27 +1155,21 @@ public class CorrelateGpxWithImages extends AbstractAction {
                 WayPoint prevWp = null;
 
                 for (WayPoint curWp : segment.getWayPoints()) {
-
-                    String curWpTimeStr = curWp.getString(GpxConstants.PT_TIME);
-                    if (curWpTimeStr != null) {
-
-                        try {
-                            long curWpTime = dateParser.parse(curWpTimeStr).getTime() + offset;
+                    try {
+                        final Date parsedTime = curWp.setTimeFromAttribute();
+                        if (parsedTime != null) {
+                            final long curWpTime = parsedTime.getTime() + offset;
                             ret += matchPoints(images, prevWp, prevWpTime, curWp, curWpTime, offset);
 
                             prevWp = curWp;
                             prevWpTime = curWpTime;
-
-                        } catch (ParseException e) {
-                            Main.error("Error while parsing date \"" + curWpTimeStr + '"');
-                            Main.error(e);
-                            prevWp = null;
-                            prevWpTime = 0;
+                            continue;
                         }
-                    } else {
-                        prevWp = null;
-                        prevWpTime = 0;
+                    } catch (Exception e) {
+                        Main.warn(e);
                     }
+                    prevWp = null;
+                    prevWpTime = 0;
                 }
             }
         }

test/unit/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImagesTest.java
@@ -1,0 +1,19 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.layer.geoimage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openstreetmap.josm.data.gpx.GpxData;
+import org.openstreetmap.josm.io.GpxReaderTest;
+
+public class CorrelateGpxWithImagesTest {
+
+    @Test
+    public void testMatchGpxTrack() throws Exception {
+        final GpxData munich = GpxReaderTest.parseGpxData("data_nodist/munich.gpx");
+        final ImageEntry i1 = new ImageEntry();
+        i1.setExifGpsTime();
+        CorrelateGpxWithImages.matchGpxTrack(null, munich, 0);
+    }
+}

src/org/openstreetmap/josm/data/gpx/WayPoint.java
@@ -11,16 +11,10 @@ import org.openstreetmap.josm.actions.search.SearchCompiler.Match;
 import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.projection.Projections;
-import org.openstreetmap.josm.tools.date.PrimaryDateParser;
+import org.openstreetmap.josm.tools.date.DateUtils;
 import org.openstreetmap.josm.tools.template_engine.TemplateEngineDataProvider;
 
 public class WayPoint extends WithAttributes implements Comparable<WayPoint>, TemplateEngineDataProvider {
-
-    private static ThreadLocal<PrimaryDateParser> dateParser = new ThreadLocal<PrimaryDateParser>() {
-        @Override protected PrimaryDateParser initialValue() {
-            return new PrimaryDateParser();
-        }
-    };
 
     public double time;
     public Color customColoring;
@@ -98,16 +92,40 @@ public class WayPoint extends WithAttributes implements Comparable<WayPoint>, Te
     }
 
     /**
+     * Sets the {@link #time} field as well as the {@link #PT_TIME} attribute to the specified time
+     *
+     * @param time the time to set
+     * @since 9383
+     */
+    public void setTime(Date time) {
+        this.time = time.getTime() / 1000.;
+        this.attr.put(PT_TIME, DateUtils.fromDate(time));
+    }
+
+    /**
      * Convert the time stamp of the waypoint into seconds from the epoch
      */
     public void setTime() {
+        setTimeFromAttribute();
+    }
+
+    /**
+     * Convert the time stamp of the waypoint into seconds from the epoch
+     * @return The parsed time if successful, or {@code null}
+     * @since 9383
+     */
+    public Date setTimeFromAttribute() {
         if (attr.containsKey(PT_TIME)) {
             try {
-                time = dateParser.get().parse(get(PT_TIME).toString()).getTime() / 1000.; /* ms => seconds */
+                final Date time = DateUtils.fromString(get(PT_TIME).toString());
+                setTime(time);
+                return time;
             } catch (Exception e) {
+                Main.warn(e);
                 time = 0;
             }
         }
+        return null;
     }
 
     @Override

src/org/openstreetmap/josm/gui/layer/geoimage/ImageEntry.java
@@ -4,7 +4,6 @@ package org.openstreetmap.josm.gui.layer.geoimage;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
@@ -448,7 +447,7 @@ public final class ImageEntry implements Comparable<ImageEntry>, Cloneable {
         // of person having time that couldn't be parsed, but valid GPS info
         try {
             setExifTime(ExifReader.readTime(file));
-        } catch (ParseException ex) {
+        } catch (RuntimeException ex) {
             setExifTime(null);
         }
 

src/org/openstreetmap/josm/tools/ExifReader.java
@@ -4,12 +4,11 @@ package org.openstreetmap.josm.tools;
 import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.Date;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.coor.LatLon;
-import org.openstreetmap.josm.tools.date.PrimaryDateParser;
+import org.openstreetmap.josm.tools.date.DateUtils;
 
 import com.drew.imaging.jpeg.JpegMetadataReader;
 import com.drew.imaging.jpeg.JpegProcessingException;
@@ -37,9 +36,8 @@ public final class ExifReader {
      * Returns the date/time from the given JPEG file.
      * @param filename The JPEG file to read
      * @return The date/time read in the EXIF section, or {@code null} if not found
-     * @throws ParseException if {@link PrimaryDateParser#parse} fails to parse date/time
      */
-    public static Date readTime(File filename) throws ParseException {
+    public static Date readTime(File filename) {
         try {
             Metadata metadata = JpegMetadataReader.readMetadata(filename);
             String dateStr = null;
@@ -59,10 +57,8 @@ public final class ExifReader {
             }
             if (dateStr != null) {
                 dateStr = dateStr.replace('/', ':'); // workaround for HTC Sensation bug, see #7228
-                return new PrimaryDateParser().parse(dateStr);
+                return DateUtils.fromString(dateStr);
             }
-        } catch (ParseException e) {
-            throw e;
         } catch (Exception e) {
             Main.error(e);
         }

src/org/openstreetmap/josm/tools/date/DateUtils.java
@@ -44,11 +44,13 @@ public final class DateUtils {
      * The shared instance is used because the construction, together
      * with the timezone lookup, is very expensive.
      */
-    private static GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+    private static final GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+    private static final GregorianCalendar calendarLocale = new GregorianCalendar(TimeZone.getDefault());
     private static final DatatypeFactory XML_DATE;
 
     static {
         calendar.setTimeInMillis(0);
+        calendarLocale.setTimeInMillis(0);
 
         DatatypeFactory fact = null;
         try {
@@ -77,10 +79,12 @@ public final class DateUtils {
         // "2007-07-25T09:26:24{Z|{+|-}01:00}"
         if (checkLayout(str, "xxxx-xx-xxTxx:xx:xxZ") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx") ||
+                checkLayout(str, "xxxx:xx:xx xx:xx:xx") ||
                 checkLayout(str, "xxxx-xx-xx xx:xx:xx UTC") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx+xx:00") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx-xx:00")) {
-            calendar.set(
+            final Calendar c = checkLayout(str, "xxxx:xx:xx xx:xx:xx") ? calendarLocale : calendar; // consider EXIF date in default timezone
+            c.set(
                 parsePart4(str, 0),
                 parsePart2(str, 5)-1,
                 parsePart2(str, 8),
@@ -91,15 +95,17 @@ public final class DateUtils {
             if (str.length() == 25) {
                 int plusHr = parsePart2(str, 20);
                 int mul = str.charAt(19) == '+' ? -3600000 : 3600000;
-                return calendar.getTimeInMillis()+plusHr*mul;
+                return c.getTimeInMillis()+plusHr*mul;
             }
 
-            return calendar.getTimeInMillis();
+            return c.getTimeInMillis();
         } else if (checkLayout(str, "xxxx-xx-xxTxx:xx:xx.xxxZ") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx.xxx") ||
+                checkLayout(str, "xxxx:xx:xx xx:xx:xx.xxx") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx.xxx+xx:00") ||
                 checkLayout(str, "xxxx-xx-xxTxx:xx:xx.xxx-xx:00")) {
-            calendar.set(
+            final Calendar c = checkLayout(str, "xxxx:xx:xx xx:xx:xx.xxx") ? calendarLocale : calendar; // consider EXIF date in default timezone
+            c.set(
                 parsePart4(str, 0),
                 parsePart2(str, 5)-1,
                 parsePart2(str, 8),
@@ -111,7 +117,7 @@ public final class DateUtils {
                 millis += parsePart2(str, 24) * (str.charAt(23) == '+' ? -3600000 : 3600000);
             }
 
-            return calendar.getTimeInMillis() + millis;
+            return c.getTimeInMillis() + millis;
         } else {
             // example date format "18-AUG-08 13:33:03"
             SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yy HH:mm:ss");

src/org/openstreetmap/josm/tools/date/PrimaryDateParser.java
@@ -16,7 +16,9 @@ import javax.xml.datatype.DatatypeFactory;
  * instance must be created per thread.
  *
  * @author Brett Henderson
+ * @deprecated Use {@link DateUtils} instead!
  */
+@Deprecated
 public class PrimaryDateParser {
     private DatatypeFactory datatypeFactory;
     private final FallbackDateParser fallbackDateParser;

test/unit/org/openstreetmap/josm/tools/date/DateUtilsTest.java
@@ -3,12 +3,23 @@ package org.openstreetmap.josm.tools.date;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.TimeZone;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Unit tests of {@link DateUtils} class.
  */
 public class DateUtilsTest {
+
+    /**
+     * Setup test.
+     */
+    @BeforeClass
+    public static void setUp() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8:00"));
+    }
 
     /**
      * Test to parse date as returned for map data.
@@ -24,5 +35,33 @@ public class DateUtilsTest {
     @Test
     public void testNoteDate() {
         assertEquals(1417298930000L, DateUtils.fromString("2014-11-29 22:08:50 UTC").getTime());
+    }
+
+    /**
+     * Test to parse date as used in EXIF structures.
+     */
+    @Test
+    public void testExifDate() {
+        assertEquals(1443038712000L - 8 * 3600 * 1000, DateUtils.fromString("2015:09:23 20:05:12").getTime());
+        assertEquals(1443038712888L - 8 * 3600 * 1000, DateUtils.fromString("2015:09:23 20:05:12.888").getTime());
+    }
+
+    /**
+     * Test to parse date as used in GPX files
+     */
+    @Test
+    public void testGPXDate() {
+        assertEquals(1277465405000L, DateUtils.fromString("2010-06-25T11:30:05.000Z").getTime());
+    }
+
+    /**
+     * Test to parse date as defined in <a href="https://tools.ietf.org/html/rfc3339">RFC 3339</a>
+     */
+    @Test
+    public void testRfc3339() {
+        // examples taken from RFC
+        assertEquals(482196050520L, DateUtils.fromString("1985-04-12T23:20:50.52Z").getTime());
+        assertEquals(851042397000L, DateUtils.fromString("1996-12-19T16:39:57-08:00").getTime());
+        assertEquals(-1041337172130L, DateUtils.fromString("1937-01-01T12:00:27.87+00:20").getTime());
     }
 }

commit 2ac6d5bb321b6f0e83bfa0c6d723e8270d8e5ac0

see #12485 - Wrong GPX Correlation, add unit test
Regression from r9383, confusion between s and ms.

git-svn-id: https://josm.openstreetmap.de/svn/trunk@9726 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImages.java
@@ -79,6 +79,7 @@ import org.openstreetmap.josm.io.JpgImporter;
 import org.openstreetmap.josm.tools.ExifReader;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Pair;
 import org.openstreetmap.josm.tools.Utils;
 import org.openstreetmap.josm.tools.date.DateUtils;
 import org.xml.sax.SAXException;
@@ -999,6 +1000,64 @@ public class CorrelateGpxWithImages extends AbstractAction {
         }
     }
 
+    static class NoGpxTimestamps extends Exception {
+    }
+
+    /**
+     * Tries to auto-guess the timezone and offset.
+     *
+     * @param imgs the images to correlate
+     * @param gpx the gpx track to correlate to
+     * @return a pair of timezone (in hours) and offset (in seconds)
+     * @throws IndexOutOfBoundsException when there are no images
+     * @throws NoGpxTimestamps when the gpx track does not contain a timestamp
+     */
+    static Pair<Double, Long> autoGuess(List<ImageEntry> imgs, GpxData gpx) throws IndexOutOfBoundsException, NoGpxTimestamps {
+
+        // Init variables
+        long firstExifDate = imgs.get(0).getExifTime().getTime() / 1000;
+
+        long firstGPXDate = -1;
+        // Finds first GPX point
+        outer: for (GpxTrack trk : gpx.tracks) {
+            for (GpxTrackSegment segment : trk.getSegments()) {
+                for (WayPoint curWp : segment.getWayPoints()) {
+                    try {
+                        final Date parsedTime = curWp.setTimeFromAttribute();
+                        if (parsedTime != null) {
+                            firstGPXDate = parsedTime.getTime() / 1000;
+                            break outer;
+                        }
+                    } catch (Exception e) {
+                        Main.warn(e);
+                    }
+                }
+            }
+        }
+
+        if (firstGPXDate < 0) {
+            throw new NoGpxTimestamps();
+        }
+
+        // seconds
+        long diff = firstExifDate - firstGPXDate;
+
+        double diffInH = (double) diff / (60 * 60);    // hours
+
+        // Find day difference
+        int dayOffset = (int) Math.round(diffInH / 24); // days
+        double tz = diff - dayOffset * 24 * 60 * 60L;  // seconds
+
+        // In hours, rounded to two decimal places
+        tz = (double) Math.round(tz * 100 / (60 * 60)) / 100;
+
+        // Due to imprecise clocks we might get a "+3:28" timezone, which should obviously be 3:30 with
+        // -2 minutes offset. This determines the real timezone and finds offset.
+        final double timezone = (double) Math.round(tz * 2) / 2; // hours, rounded to one decimal place
+        final long delta = Math.round(diff - timezone * 60 * 60); // seconds
+        return Pair.create(timezone, delta);
+    }
+
     private class AutoGuessActionListener implements ActionListener {
 
         @Override
@@ -1010,59 +1069,21 @@ public class CorrelateGpxWithImages extends AbstractAction {
 
             List<ImageEntry> imgs = getSortedImgList();
 
-            // no images found, exit
-            if (imgs.isEmpty()) {
+            try {
+                final Pair<Double, Long> r = autoGuess(imgs, gpx);
+                timezone = r.a;
+                delta = r.b;
+            } catch (IndexOutOfBoundsException ex) {
                 JOptionPane.showMessageDialog(Main.parent,
                         tr("The selected photos do not contain time information."),
                         tr("Photos do not contain time information"), JOptionPane.WARNING_MESSAGE);
                 return;
-            }
-
-            // Init variables
-            long firstExifDate = imgs.get(0).getExifTime().getTime()/1000;
-
-            long firstGPXDate = -1;
-            // Finds first GPX point
-            outer: for (GpxTrack trk : gpx.tracks) {
-                for (GpxTrackSegment segment : trk.getSegments()) {
-                    for (WayPoint curWp : segment.getWayPoints()) {
-                        try {
-                            final Date parsedTime = curWp.setTimeFromAttribute();
-                            if (parsedTime != null) {
-                                firstGPXDate = parsedTime.getTime();
-                                break outer;
-                            }
-                        } catch (Exception e) {
-                            Main.warn(e);
-                        }
-                    }
-                }
-            }
-
-            // No GPX timestamps found, exit
-            if (firstGPXDate < 0) {
+            } catch (NoGpxTimestamps ex) {
                 JOptionPane.showMessageDialog(Main.parent,
                         tr("The selected GPX track does not contain timestamps. Please select another one."),
                         tr("GPX Track has no time information"), JOptionPane.WARNING_MESSAGE);
                 return;
             }
-
-            // seconds
-            long diff = firstExifDate - firstGPXDate;
-
-            double diffInH = (double) diff/(60*60);    // hours
-
-            // Find day difference
-            int dayOffset = (int) Math.round(diffInH / 24); // days
-            double tz = diff - dayOffset*24*60*60L;  // seconds
-
-            // In hours, rounded to two decimal places
-            tz = (double) Math.round(tz*100/(60*60)) / 100;
-
-            // Due to imprecise clocks we might get a "+3:28" timezone, which should obviously be 3:30 with
-            // -2 minutes offset. This determines the real timezone and finds offset.
-            timezone = (double) Math.round(tz * 2)/2; // hours, rounded to one decimal place
-            delta = Math.round(diff - timezone*60*60); // seconds
 
             tfTimezone.getDocument().removeDocumentListener(statusBarUpdater);
             tfOffset.getDocument().removeDocumentListener(statusBarUpdater);

test/unit/org/openstreetmap/josm/gui/layer/geoimage/CorrelateGpxWithImagesTest.java
@@ -4,6 +4,7 @@ package org.openstreetmap.josm.gui.layer.geoimage;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.TimeZone;
 
 import org.junit.BeforeClass;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.gpx.GpxData;
 import org.openstreetmap.josm.io.GpxReaderTest;
+import org.openstreetmap.josm.tools.Pair;
 import org.openstreetmap.josm.tools.date.DateUtils;
 import org.openstreetmap.josm.tools.date.DateUtilsTest;
 
@@ -57,5 +59,18 @@ public class CorrelateGpxWithImagesTest {
         assertEquals(new LatLon(47.197319911792874, 8.792139580473304), i3.getPos()); // exact match
         assertEquals(new LatLon((47.197131179273129 + 47.197186248376966) / 2, (8.792974585667253 + 8.792809881269932) / 2),
                 i2.getPos()); // interpolated
+    }
+
+    /**
+     * Tests automatic guessing of timezone/offset
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void testAutoGuess() throws Exception {
+        final GpxData gpx = GpxReaderTest.parseGpxData("data_nodist/2094047.gpx");
+        final ImageEntry i0 = new ImageEntry();
+        i0.setExifTime(DateUtils.fromString("2016:01:03 11:59:54")); // 4 sec before start of GPX
+        i0.createTmp();
+        assertEquals(Pair.create(0.0, -4L), CorrelateGpxWithImages.autoGuess(Collections.singletonList(i0), gpx));
     }
 }
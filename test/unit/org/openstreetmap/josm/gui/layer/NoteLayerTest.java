wiktorn/myjosm

commit 4bc78eb987429fc4badf9f5a074870564dc63c81

sonar - fb-contrib - minor performance improvements: * Method passes constant String of length 1 to character overridden method * Method needlessly boxes a boolean constant * Method uses iterator().next() on a List to get the first item * Method converts String to boxed primitive using excessive boxing * Method converts String to primitive using excessive boxing * Method creates array using constants * Class defines List based fields but uses them like Sets
git-svn-id: https://josm.openstreetmap.de/svn/trunk@8846 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/NoteLayer.java
@@ -179,7 +179,7 @@ public class NoteLayer extends AbstractModifiableLayer implements MouseListener 
 
     @Override
     public String getToolTipText() {
-        return noteData.getNotes().size() + " " + tr("Notes");
+        return noteData.getNotes().size() + ' ' + tr("Notes");
     }
 
     @Override

commit 20e887b4bf3e4a16368d9ad62048b01014b23f4b

fix #13208 - Use real string for concatenation in note layer tooltip (patch by michael2402) + add unit test (regression from r8846) - gsoc-core
git-svn-id: https://josm.openstreetmap.de/svn/trunk@10640 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/NoteLayer.java
@@ -187,7 +187,7 @@ public class NoteLayer extends AbstractModifiableLayer implements MouseListener 
 
     @Override
     public String getToolTipText() {
-        return noteData.getNotes().size() + ' ' + tr("Notes");
+        return noteData.getNotes().size() + " " + tr("Notes");
     }
 
     @Override

test/unit/org/openstreetmap/josm/gui/layer/NoteLayerTest.java
@@ -1,0 +1,30 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.layer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openstreetmap.josm.JOSMFixture;
+
+/**
+ * Unit tests of {@link NoteLayer} class.
+ */
+public class NoteLayerTest {
+
+    /**
+     * Setup tests
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        JOSMFixture.createUnitTestFixture().init();
+    }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/13208">#13208</a>.
+     */
+    @Test
+    public void testTicket13208() {
+        assertEquals("0 Notes", new NoteLayer().getToolTipText());
+    }
+}
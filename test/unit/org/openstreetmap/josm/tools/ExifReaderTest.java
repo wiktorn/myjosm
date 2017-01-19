wiktorn/myjosm

commit cc492b9a3ab33f952a9e23b6e0e53e6b8aed7996

fix #11685 - Use sub-second exif/xmp data
git-svn-id: https://josm.openstreetmap.de/svn/trunk@9499 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/ExifReader.java
@@ -41,23 +41,37 @@ public final class ExifReader {
         try {
             Metadata metadata = JpegMetadataReader.readMetadata(filename);
             String dateStr = null;
-            OUTER:
+            String subSeconds = null;
             for (Directory dirIt : metadata.getDirectories()) {
                 for (Tag tag : dirIt.getTags()) {
                     if (tag.getTagType() == ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL /* 0x9003 */ &&
                             !tag.getDescription().matches("\\[[0-9]+ .+\\]")) {
                         dateStr = tag.getDescription();
-                        break OUTER; // prefer this tag if known
                     }
                     if (tag.getTagType() == ExifIFD0Directory.TAG_DATETIME /* 0x0132 */ ||
                         tag.getTagType() == ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED /* 0x9004 */) {
-                        dateStr = tag.getDescription();
+                        if (dateStr != null) {
+                            // prefer TAG_DATETIME_ORIGINAL
+                            dateStr = tag.getDescription();
+                        }
+                    }
+                    if (tag.getTagType() == ExifIFD0Directory.TAG_SUBSECOND_TIME_ORIGINAL) {
+                        subSeconds = tag.getDescription();
                     }
                 }
             }
             if (dateStr != null) {
                 dateStr = dateStr.replace('/', ':'); // workaround for HTC Sensation bug, see #7228
-                return DateUtils.fromString(dateStr);
+                final Date date = DateUtils.fromString(dateStr);
+                if (subSeconds != null) {
+                    try {
+                        date.setTime(date.getTime() + Integer.parseInt(subSeconds));
+                    } catch (NumberFormatException e) {
+                        Main.warn("Failed parsing sub seconds from [{0}]", subSeconds);
+                        Main.warn(e);
+                    }
+                }
+                return date;
             }
         } catch (Exception e) {
             Main.error(e);

test/unit/org/openstreetmap/josm/tools/ExifReaderTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertNotNull;
 import java.io.File;
 import java.text.DecimalFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -43,6 +44,17 @@ public class ExifReaderTest {
     public void testReadTime() throws ParseException {
         Date date = ExifReader.readTime(directionSampleFile);
         assertEquals(new GregorianCalendar(2010, Calendar.MAY, 15, 17, 12, 05).getTime(), date);
+    }
+
+    /**
+     * Tests reading sub-seconds from the EXIF header
+     * @throws ParseException  if {@link ExifReader#readTime} fails to parse date/time of sample file
+     */
+    @Test
+    public void testReadTimeSubSecond1() throws ParseException {
+        Date date = ExifReader.readTime(new File("data_nodist/IMG_20150711_193419.jpg"));
+        String dateStr = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").format(date);
+        assertEquals("2015-07-11T19:34:19.100", dateStr);
     }
 
     /**

commit 32e457eff0dc5b896e0b92926e95ac28a70934ec

fix #14209 - prefer TAG_DATETIME_ORIGINAL (patch by holgermappt, regression from r9499) + add non-regression unit test
git-svn-id: https://josm.openstreetmap.de/svn/trunk@11484 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/tools/ExifReader.java
@@ -55,7 +55,7 @@ public final class ExifReader {
                     }
                     if (tag.getTagType() == ExifIFD0Directory.TAG_DATETIME /* 0x0132 */ ||
                         tag.getTagType() == ExifSubIFDDirectory.TAG_DATETIME_DIGITIZED /* 0x9004 */) {
-                        if (dateStr != null) {
+                        if (dateStr == null) {
                             // prefer TAG_DATETIME_ORIGINAL
                             dateStr = tag.getDescription();
                         }

test/unit/org/openstreetmap/josm/tools/ExifReaderTest.java
@@ -118,4 +118,15 @@ public class ExifReaderTest {
         String dateStr = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").format(ExifReader.readTime(file));
         assertEquals("2015-11-08T15:33:27.500", dateStr);
     }
+
+    /**
+     * Non-regression test for ticket <a href="https://josm.openstreetmap.de/ticket/14209">#14209</a>
+     * @throws IOException if an error occurs during reading
+     */
+    @Test
+    public void testTicket14209() throws IOException {
+        File file = new File(TestUtils.getRegressionDataFile(14209, "0MbEfj1S--.1.jpg"));
+        String dateStr = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").format(ExifReader.readTime(file));
+        assertEquals("2017-01-16T18:27:00.000", dateStr);
+    }
 }
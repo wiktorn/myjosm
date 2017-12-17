wiktorn/myjosm

commit 50502f3513cf6e466585a1ed5f7bcb6816c993c0

applied #8509 - Background uploading (patch by udit, minor changes)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13133 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/MainLayerManager.java
@@ -1,13 +1,18 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.gui.layer;
 
+import static org.openstreetmap.josm.tools.I18n.tr;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.swing.JOptionPane;
+
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.util.GuiHelper;
 
 /**
@@ -205,13 +210,23 @@ public class MainLayerManager extends LayerManager {
     }
 
     /**
-     * Set the active layer. If the layer is an OsmDataLayer, the edit layer is also changed.
+     * Set the active layer, unless the layer is read-only.
+     * If the layer is an OsmDataLayer, the edit layer is also changed.
      * @param layer The active layer.
      */
     public void setActiveLayer(final Layer layer) {
         // we force this on to the EDT Thread to make events fire from there.
         // The synchronization lock needs to be held by the EDT.
-        GuiHelper.runInEDTAndWaitWithException(() -> realSetActiveLayer(layer));
+        if (layer instanceof OsmDataLayer && ((OsmDataLayer) layer).isReadOnly()) {
+            GuiHelper.runInEDT(() ->
+                    JOptionPane.showMessageDialog(
+                            MainApplication.parent,
+                            tr("Trying to set a read only data layer as edit layer"),
+                            tr("Warning"),
+                            JOptionPane.WARNING_MESSAGE));
+        } else {
+            GuiHelper.runInEDTAndWaitWithException(() -> realSetActiveLayer(layer));
+        }
     }
 
     protected synchronized void realSetActiveLayer(final Layer layer) {
@@ -309,16 +324,27 @@ public class MainLayerManager extends LayerManager {
      * @return the currently active layer (may be null)
      */
     public synchronized Layer getActiveLayer() {
-        return activeLayer;
+        if (activeLayer instanceof OsmDataLayer) {
+            if (!((OsmDataLayer) activeLayer).isReadOnly()) {
+                return activeLayer;
+            } else {
+                return null;
+            }
+        } else {
+            return activeLayer;
+        }
     }
 
     /**
-     * Replies the current edit layer, if any
+     * Replies the current edit layer, if present and not readOnly
      *
      * @return the current edit layer. May be null.
      */
     public synchronized OsmDataLayer getEditLayer() {
-        return editLayer;
+        if (editLayer != null && !editLayer.isReadOnly())
+            return editLayer;
+        else
+            return null;
     }
 
     /**
@@ -377,5 +403,45 @@ public class MainLayerManager extends LayerManager {
 
         activeLayerChangeListeners.clear();
         layerAvailabilityListeners.clear();
+    }
+
+    /**
+     * Prepares an OsmDataLayer for upload. The layer to be uploaded is locked and
+     * if the layer to be uploaded is the current editLayer then editLayer is reset
+     * to null for disallowing any changes to the layer. An ActiveLayerChangeEvent
+     * is fired to notify the listeners
+     *
+     * @param layer The OsmDataLayer to be uploaded
+     */
+    public void prepareLayerForUpload(OsmDataLayer layer) {
+
+        GuiHelper.assertCallFromEdt();
+        layer.setReadOnly();
+
+        // Reset only the edit layer as empty
+        if (editLayer == layer) {
+            ActiveLayerChangeEvent activeLayerChangeEvent = new ActiveLayerChangeEvent(this, editLayer, activeLayer);
+            editLayer = null;
+            fireActiveLayerChange(activeLayerChangeEvent);
+        }
+    }
+
+    /**
+     * Post upload processing of the OsmDataLayer.
+     * If the current edit layer is empty this function sets the layer uploaded as the
+     * current editLayer. An ActiveLayerChangeEvent is fired to notify the listeners
+     *
+     * @param layer The OsmDataLayer uploaded
+     */
+    public void processLayerAfterUpload(OsmDataLayer layer) {
+        GuiHelper.assertCallFromEdt();
+        layer.unsetReadOnly();
+
+        // Set the layer as edit layer if the edit layer is empty.
+        if (editLayer == null) {
+            ActiveLayerChangeEvent layerChangeEvent = new ActiveLayerChangeEvent(this, editLayer, activeLayer);
+            editLayer = layer;
+            fireActiveLayerChange(layerChangeEvent);
+        }
     }
 }

src/org/openstreetmap/josm/actions/UploadAction.java
@@ -9,6 +9,7 @@ import java.awt.event.KeyEvent;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.swing.JOptionPane;
 
@@ -24,11 +25,13 @@ import org.openstreetmap.josm.data.conflict.ConflictCollection;
 import org.openstreetmap.josm.data.osm.Changeset;
 import org.openstreetmap.josm.gui.HelpAwareOptionPane;
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.io.AsynchronousUploadPrimitivesTask;
 import org.openstreetmap.josm.gui.io.UploadDialog;
 import org.openstreetmap.josm.gui.io.UploadPrimitivesTask;
 import org.openstreetmap.josm.gui.layer.AbstractModifiableLayer;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.gui.util.GuiHelper;
+import org.openstreetmap.josm.spi.preferences.Config;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Shortcut;
 import org.openstreetmap.josm.tools.Utils;
@@ -56,6 +59,8 @@ public class UploadAction extends JosmAction {
      */
     private static final List<UploadHook> UPLOAD_HOOKS = new LinkedList<>();
     private static final List<UploadHook> LATE_UPLOAD_HOOKS = new LinkedList<>();
+
+    private static final String IS_ASYNC_UPLOAD_ENABLED = "asynchronous.upload";
 
     static {
         /**
@@ -259,14 +264,24 @@ public class UploadAction extends JosmAction {
             hook.modifyChangesetTags(changesetTags);
         }
 
-        MainApplication.worker.execute(
-                new UploadPrimitivesTask(
-                        UploadDialog.getUploadDialog().getUploadStrategySpecification(),
-                        layer,
-                        apiData,
-                        cs
-                )
-        );
+        if (Config.getPref().getBoolean(IS_ASYNC_UPLOAD_ENABLED, true)) {
+            Optional<AsynchronousUploadPrimitivesTask> asyncUploadTask = AsynchronousUploadPrimitivesTask.createAsynchronousUploadTask(
+                    UploadDialog.getUploadDialog().getUploadStrategySpecification(),
+                    layer,
+                    apiData,
+                    cs);
+
+            if (asyncUploadTask.isPresent()) {
+                MainApplication.worker.execute(asyncUploadTask.get());
+            }
+        } else {
+            MainApplication.worker.execute(
+                    new UploadPrimitivesTask(
+                            UploadDialog.getUploadDialog().getUploadStrategySpecification(),
+                            layer,
+                            apiData,
+                            cs));
+        }
     }
 
     @Override

src/org/openstreetmap/josm/gui/io/AsynchronousUploadPrimitivesTask.java
@@ -1,0 +1,153 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.io;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.GraphicsEnvironment;
+import java.util.Optional;
+
+import javax.swing.JOptionPane;
+
+import org.openstreetmap.josm.data.APIDataSet;
+import org.openstreetmap.josm.data.osm.Changeset;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.dialogs.LayerListDialog;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.gui.progress.ProgressTaskId;
+import org.openstreetmap.josm.gui.util.GuiHelper;
+import org.openstreetmap.josm.io.UploadStrategySpecification;
+
+/**
+ * Task for uploading primitives using background worker threads. The actual upload is delegated to the
+ * {@link UploadPrimitivesTask}. This class is a wrapper over that to make the background upload process safe. There
+ * can only be one instance of this class, hence background uploads are limited to one at a time. This class also
+ * changes the editLayer of {@link org.openstreetmap.josm.gui.layer.MainLayerManager} to null during upload so that
+ * any changes to the uploading layer are prohibited.
+ *
+ * @author udit
+ * @since 13133
+ */
+public final class AsynchronousUploadPrimitivesTask extends UploadPrimitivesTask {
+
+    /**
+     * Static instance
+     */
+    private static AsynchronousUploadPrimitivesTask asynchronousUploadPrimitivesTask = null;
+
+    /**
+     * Member fields
+     */
+    private final ProgressTaskId taskId;
+    private final OsmDataLayer uploadDataLayer;
+
+    /**
+     * Private constructor to restrict creating more Asynchronous upload tasks
+     *
+     * @param uploadStrategySpecification UploadStrategySpecification for the DataLayer
+     * @param osmDataLayer Datalayer to be uploaded
+     * @param apiDataSet ApiDataSet that contains the primitives to be uploaded
+     * @param changeset Changeset for the datalayer
+     *
+     * @throws IllegalArgumentException if layer is null
+     * @throws IllegalArgumentException if toUpload is null
+     * @throws IllegalArgumentException if strategy is null
+     * @throws IllegalArgumentException if changeset is null
+     */
+    private AsynchronousUploadPrimitivesTask(UploadStrategySpecification uploadStrategySpecification,
+                                             OsmDataLayer osmDataLayer, APIDataSet apiDataSet, Changeset changeset) {
+        super(uploadStrategySpecification,
+                osmDataLayer,
+                apiDataSet,
+                changeset);
+
+        uploadDataLayer = osmDataLayer;
+        // Create a ProgressTaskId for background upload
+        taskId = new ProgressTaskId("core", "async-upload");
+    }
+
+    /**
+     * Creates an instance of AsynchronousUploadPrimitiveTask
+     *
+     * @param uploadStrategySpecification UploadStrategySpecification for the DataLayer
+     * @param dataLayer Datalayer to be uploaded
+     * @param apiDataSet ApiDataSet that contains the primitives to be uploaded
+     * @param changeset Changeset for the datalayer
+     * @return Returns an {@literal Optional<AsynchronousUploadPrimitivesTask> } if there is no
+     * background upload in progress. Otherwise returns an {@literal Optional.empty()}
+     *
+     * @throws IllegalArgumentException if layer is null
+     * @throws IllegalArgumentException if toUpload is null
+     * @throws IllegalArgumentException if strategy is null
+     * @throws IllegalArgumentException if changeset is null
+     */
+    public static Optional<AsynchronousUploadPrimitivesTask> createAsynchronousUploadTask(
+            UploadStrategySpecification uploadStrategySpecification,
+             OsmDataLayer dataLayer, APIDataSet apiDataSet, Changeset changeset) {
+        synchronized (AsynchronousUploadPrimitivesTask.class) {
+            if (asynchronousUploadPrimitivesTask != null) {
+                if (!GraphicsEnvironment.isHeadless()) {
+                    GuiHelper.runInEDTAndWait(() ->
+                            JOptionPane.showMessageDialog(MainApplication.parent,
+                                    tr("A background upload is already in progress. " +
+                                            "Kindly wait for it to finish before uploading new changes")));
+                }
+                return Optional.empty();
+            } else {
+                // Create an asynchronous upload task
+                asynchronousUploadPrimitivesTask = new AsynchronousUploadPrimitivesTask(
+                        uploadStrategySpecification,
+                        dataLayer,
+                        apiDataSet,
+                        changeset);
+                return Optional.ofNullable(asynchronousUploadPrimitivesTask);
+            }
+        }
+    }
+
+    /**
+     * Get the current upload task
+     * @return {@literal Optional<AsynchronousUploadPrimitivesTask> }
+     */
+    public static Optional<AsynchronousUploadPrimitivesTask> getCurrentAsynchronousUploadTask() {
+        return Optional.ofNullable(asynchronousUploadPrimitivesTask);
+    }
+
+    @Override
+    public ProgressTaskId canRunInBackground() {
+        return taskId;
+    }
+
+    @Override
+    protected void realRun() {
+        // Lock the data layer before upload in EDT
+        GuiHelper.runInEDTAndWait(() -> {
+            // Remove the commands from the undo stack
+            MainApplication.undoRedo.clean(uploadDataLayer.data);
+            MainApplication.getLayerManager().prepareLayerForUpload(uploadDataLayer);
+
+            // Repainting the Layer List dialog to update the icon of the active layer
+            LayerListDialog.getInstance().repaint();
+        });
+        super.realRun();
+    }
+
+    @Override
+    protected void cancel() {
+        super.cancel();
+        asynchronousUploadPrimitivesTask = null;
+    }
+
+    @Override
+    protected void finish() {
+        try {
+            // Unlock the data layer in EDT
+            GuiHelper.runInEDTAndWait(() -> {
+                MainApplication.getLayerManager().processLayerAfterUpload(uploadDataLayer);
+                LayerListDialog.getInstance().repaint();
+            });
+            super.finish();
+        } finally {
+            asynchronousUploadPrimitivesTask = null;
+        }
+    }
+}

src/org/openstreetmap/josm/gui/layer/OsmDataLayer.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
@@ -129,6 +130,8 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, D
 
     private boolean requiresSaveToFile;
     private boolean requiresUploadToServer;
+    /** Flag used to know if the layer should not be editable */
+    private final AtomicBoolean isReadOnly = new AtomicBoolean(false);
 
     /**
      * List of validation errors in this layer.
@@ -419,6 +422,11 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, D
         ImageProvider base = getBaseIconProvider().setMaxSize(ImageSizes.LAYER);
         if (isUploadDiscouraged() || data.getUploadPolicy() == UploadPolicy.BLOCKED) {
             base.addOverlay(new ImageOverlay(new ImageProvider("warning-small"), 0.5, 0.5, 1.0, 1.0));
+        }
+
+        if (isReadOnly()) {
+            // If the layer is read only then change the default icon to a clock
+            base = new ImageProvider("clock").setMaxSize(ImageSizes.LAYER);
         }
         return base.get();
     }
@@ -1142,5 +1150,31 @@ public class OsmDataLayer extends AbstractModifiableLayer implements Listener, D
             data.setName(name);
         }
         super.setName(name);
+    }
+
+    /**
+     * Sets the isReadOnly flag for the OsmDataLayer as true
+     */
+    public void setReadOnly() {
+        if (!isReadOnly.compareAndSet(false, true)) {
+            Logging.warn("Trying to set readOnly flag on a readOnly layer ", this.getName());
+        }
+    }
+
+    /**
+     * Sets the isReadOnly flag for the OsmDataLayer as false
+     */
+    public void unsetReadOnly() {
+        if (!isReadOnly.compareAndSet(true, false)) {
+            Logging.warn("Trying to unset readOnly flag on a non-readOnly layer ", this.getName());
+        }
+    }
+
+    /**
+     * Returns the value of the isReadOnly flag for the OsmDataLayer
+     * @return isReadOnly
+     */
+    public boolean isReadOnly() {
+        return isReadOnly.get();
     }
 }

test/unit/org/openstreetmap/josm/gui/io/AsynchronousUploadPrimitivesTaskTest.java
@@ -1,0 +1,74 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.gui.io;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.junit.Rule;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Assert;
+import org.openstreetmap.josm.data.APIDataSet;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.Changeset;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.io.UploadStrategySpecification;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import java.util.Optional;
+
+public class AsynchronousUploadPrimitivesTaskTest {
+
+    private UploadStrategySpecification strategy;
+    private OsmDataLayer layer;
+    private APIDataSet toUpload;
+    private Changeset changeset;
+    private AsynchronousUploadPrimitivesTask uploadPrimitivesTask;
+
+    /**
+     * Setup tests
+     */
+    @Rule
+    @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public JOSMTestRules test = new JOSMTestRules();
+
+    @Before
+    public void bootStrap() {
+        DataSet dataSet = new DataSet();
+        Node node1 = new Node();
+        Node node2 = new Node();
+        node1.setCoor(new LatLon(0, 0));
+        node2.setCoor(new LatLon(30, 30));
+        Way way = new Way();
+        way.addNode(node1);
+        way.addNode(node2);
+        dataSet.addPrimitive(node1);
+        dataSet.addPrimitive(node2);
+        dataSet.addPrimitive(way);
+
+        toUpload = new APIDataSet(dataSet);
+        layer = new OsmDataLayer(dataSet, "uploadTest", null);
+        strategy = new UploadStrategySpecification();
+        changeset = new Changeset();
+        uploadPrimitivesTask = AsynchronousUploadPrimitivesTask.createAsynchronousUploadTask(strategy, layer, toUpload, changeset).get();
+    }
+
+    @After
+    public void tearDown() {
+        toUpload = null;
+        layer = null;
+        strategy = null;
+        changeset = null;
+        uploadPrimitivesTask = null;
+    }
+
+    @Test
+    public void testSingleUploadInstance() {
+        Optional<AsynchronousUploadPrimitivesTask> task = AsynchronousUploadPrimitivesTask.
+                createAsynchronousUploadTask(strategy, layer, toUpload, changeset);
+        Assert.assertNotNull(uploadPrimitivesTask);
+        Assert.assertFalse(task.isPresent());
+    }
+}

commit 0a1a4a31a1b4ff770c5a3d67e9c6f250afeffbef

fix #15653, see #8509 - IllegalStateException during Upload (regression from r13133 - patch by udit, minor changes)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@13213 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/layer/MainLayerManager.java
@@ -13,6 +13,7 @@ import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.io.AsynchronousUploadPrimitivesTask;
 import org.openstreetmap.josm.gui.util.GuiHelper;
 
 /**
@@ -273,6 +274,14 @@ public class MainLayerManager extends LayerManager {
 
     @Override
     protected Collection<Layer> realRemoveSingleLayer(Layer layer) {
+        if ((layer instanceof OsmDataLayer) && (((OsmDataLayer) layer).isReadOnly())) {
+            GuiHelper.runInEDT(() -> JOptionPane.showMessageDialog(MainApplication.parent,
+                    tr("Trying to delete the layer with background upload. Please wait until the upload is finished.")));
+
+            // Return an empty collection for allowing to delete other layers
+            return new ArrayList<>();
+        }
+
         if (layer == activeLayer || layer == editLayer) {
             Layer nextActive = suggestNextActiveLayer(layer);
             setActiveLayer(nextActive, true);
@@ -408,11 +417,17 @@ public class MainLayerManager extends LayerManager {
 
     @Override
     protected synchronized void realResetState() {
-        // active and edit layer are unset automatically
-        super.realResetState();
+        // Reset state if no asynchronous upload is under progress
+        if (!AsynchronousUploadPrimitivesTask.getCurrentAsynchronousUploadTask().isPresent()) {
+            // active and edit layer are unset automatically
+            super.realResetState();
 
-        activeLayerChangeListeners.clear();
-        layerAvailabilityListeners.clear();
+            activeLayerChangeListeners.clear();
+            layerAvailabilityListeners.clear();
+        } else {
+            GuiHelper.runInEDT(() -> JOptionPane.showMessageDialog(MainApplication.parent,
+                    tr("A background upload is already in progress. Cannot reset state until the upload is finished.")));
+        }
     }
 
     /**